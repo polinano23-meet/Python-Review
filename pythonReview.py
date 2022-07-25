ee5c4f39a629af12d5ed6982f0c52f9277e652a2
polinano23-meet <109908211+polinano23-meet@users.noreply.github.com>
2022-07-25 16:01:01 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -1,7 +1,5 @@
 def create_youtube_video(title,description):
-	title=input("enter the video title")
-	description=input("enter a description for the video")
-	video={"title":title,"description":description,"likes":0,"dislikes":0,"comments":{"username":""}}
+	video={"title":title,"description":description,"likes":0,"dislikes":0,"comments":{}}
 
 	return video
 
@@ -21,12 +19,14 @@ def dislike(video):
 
 
 def add_comment(youtubevideo,username,comment_text):
-	youtubevideo["comments"][username]:comment_text
+	youtubevideo["comments"][username]=comment_text
 
 	return youtubevideo
 
 new_video = create_youtube_video("Hey world","A song's music video")
-for L in range(496):
+print(new_video)
+add_comment(new_video,"polina","hey")
+for L in range(495):
 	like(new_video)
 print(new_video)
 
